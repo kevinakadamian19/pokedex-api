d00972b0344f9bf8a01a1b32ca12996f0067d50a
kevinakadamian19 <kevindamianta@gmail.com>
2019-09-19 12:03:18 -0700

    pokedex-api
    API created while learning express. It allows users to search for Pokemon, or types of Pokemon.
    
    Pokemon can be filtered by name, or by type.

---
+++ server.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
+
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'});
+    }
+    next();
+});
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+function handleGetTypes(req,res) {
+    res.json(validTypes);
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req,res) {
+    let response = POKEDEX.pokemon;
+    
+    if(req.query.name) {
+        response = response.filter(pokemon => 
+            pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())    
+        )
+    };
+    
+    if(req.query.type) {
+        response = response.filter(pokemon =>
+            pokemon.type.includes(req.query.type)
+        )
+    }
+    res.json(response)
+}
+
+app.get('/pokemon', handleGetPokemon)
+
+app.use((error, req, res, next) => {
+    let response;
+    if(process.env.NODE_ENV === 'production') {
+        response = {error: {message: 'server error'}}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response);
+})
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {})
